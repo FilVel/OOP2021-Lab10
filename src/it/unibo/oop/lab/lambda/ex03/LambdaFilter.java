60e21dc0529d5907719145547d88eeafb15a2592
Filippo Velli <filippo.velli@studio.unibo.it>
2021-12-06 21:32:02 +0100

    Exercise ex03 complete

--- src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
+++ src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
@@ -6,7 +6,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -34,8 +36,23 @@ public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
 
+    private static final String NO_STRING = "(\\s|\\p{Punct})+";
+
     private enum Command {
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        TO_LOWERCASE("Convert to lowercase", string -> string.toLowerCase()),
+        COUNT_CHARS("Count the number of chars", string -> Integer.toString(string.length())),
+        COUNT_LINES("Count the number of lines", string -> Long.toString(string.chars().
+            filter(ch -> ch == '\n').count() + 1)),
+        ALPHABET_ORDER("List all words in alphabetical order", string -> 
+            Arrays.stream(string.split(NO_STRING))
+                .sorted().collect(Collectors.joining("\n"))),
+        COUNT_WORD("Show how many times a word has been used", string ->
+            Arrays.stream(string.split(NO_STRING))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(word -> word.getKey() + " -> " + word.getValue())
+                .collect(Collectors.joining("\n")));
 
         private final String commandName;
         private final Function<String, String> fun;
